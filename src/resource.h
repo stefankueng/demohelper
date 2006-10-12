8a2411e31af2396782f5983e0d76480a21f67693
Stefan Küng <stefankueng@gmail.com>
2006-10-12 18:11:38 +0000

    Add an own icon. The photoshop file is for the future, because currently VS2005 doesn't like vista icons and we will need one soon.

--- src/resource.h
+++ src/resource.h
@@ -8,7 +8,6 @@
 #define IDM_ABOUT                       104
 #define IDM_EXIT                        105
 #define IDI_SHOWHELPER                  107
-#define IDI_SMALL                       108
 #define IDC_SHOWHELPER                  109
 #define IDR_SHOWHELPER                  109
 #define IDR_MAINFRAME                   128
