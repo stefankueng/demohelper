6baf51ac5a808eedddb5b2d9f278b8b9b143758a
tortoisesvn <tortoisesvn@7d1c1021-785a-45e7-86c9-21184ceb001a>
2006-12-30 14:50:17 +0000

    Tag version 1.2

---
+++ tags/Version-1.2/src/MainWindow.h
@@ -0,0 +1,137 @@
+#pragma once
+#include "basewindow.h"
+#include "Resource.h"
+#include "shellapi.h"
+#include "shlwapi.h"
+#include "hyperlink.h"
+#include <commctrl.h>
+#include <vector>
+
+#define LINEARRAYSIZE 4000
+#define MAX_NUMBEROFLINES 50
+#define DRAW_HOTKEY 100
+#define ZOOM_HOTKEY 101
+
+#define TIMER_ID_DRAW	101
+#define TIMER_ID_ZOOM	102
+#define TIMER_ID_FADE	103
+
+typedef enum LineTypes
+{
+	normal,
+	arrow,
+} LineTypes;
+
+class CMainWindow :
+	public CWindow
+{
+public:
+	CMainWindow(HINSTANCE hInst, const WNDCLASSEX* wcx = NULL) : CWindow(hInst, wcx)
+		, m_bDrawing(false)
+		, m_bZooming(false)
+		, m_zoomfactor(2.0f)
+		, hDesktopCompatibleDC(NULL)
+		, hDesktopCompatibleBitmap(NULL)
+		, hOldBmp(NULL)
+		, m_totallines(-1)
+		, m_colorindex(1)
+		, m_currentpenwidth(6)
+		, m_hCursor(NULL)
+		, m_currentrop(R2_MASKPEN)
+		, m_bMarker(false)
+		, m_bInlineZoom(false)
+		, m_fadeseconds(0)
+	{
+		SetWindowTitle((LPCTSTR)ResString(hResource, IDS_APP_TITLE));
+		m_colors[0] = RGB(255, 255, 0);
+		m_colors[1] = RGB(255, 0, 0);
+		m_colors[2] = RGB(150, 0, 0);
+		m_colors[3] = RGB(0, 255, 0);
+		m_colors[4] = RGB(0, 150, 0);
+		m_colors[5] = RGB(0, 0, 255);
+		m_colors[6] = RGB(0, 0, 150);
+		m_colors[7] = RGB(0, 0, 0);
+		m_colors[8] = RGB(150, 150, 150);
+		m_colors[9] = RGB(0, 255, 255);
+		m_points = new POINT[MAX_NUMBEROFLINES*LINEARRAYSIZE];
+		m_linetypes = new BYTE[MAX_NUMBEROFLINES*LINEARRAYSIZE];
+	};
+	~CMainWindow(void)
+	{
+		delete [] m_points;
+		delete [] m_linetypes;
+	};
+
+	bool				RegisterAndCreateWindow();
+
+protected:
+	/// the message handler for this window
+	LRESULT CALLBACK	WinMsgHandler(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	/// Handles all the WM_COMMAND window messages (e.g. menu commands)
+	LRESULT				DoCommand(int id);
+
+	bool				StartPresentationMode();
+	bool				EndPresentationMode();
+	bool				StartInlineZoom();
+	bool				StartZoomingMode();
+	bool				EndZoomingMode();
+	bool				DrawArrow(HDC hdc, int index);
+	bool				ArrowTo(HDC hdc, LONG x, LONG y, int width);
+	bool				DrawZoom(HDC hdc, POINT pt);
+	HCURSOR				CreateDrawCursor(COLORREF color, int penwidth);
+
+	static BOOL CALLBACK OptionsDlgProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam);
+	static WORD			HotKeyControl2HotKey(WORD hk);
+	static WORD			HotKey2HotKeyControl(WORD hk);
+	static BOOL CALLBACK HelpDlgProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam);
+
+	void				RegisterHotKeys();
+	bool				UpdateCursor();
+	DWORD				GetDllVersion(LPCTSTR lpszDllName);
+protected:
+	NOTIFYICONDATA		niData; 
+	HDC					hDesktopCompatibleDC;
+	HBITMAP				hDesktopCompatibleBitmap;
+	HBITMAP				hOldBmp;
+
+	bool				m_bDrawing;
+	float				m_zoomfactor;
+	bool				m_bZooming;
+
+	int					m_totallines;
+	int					m_lineindex[MAX_NUMBEROFLINES];
+	POINT *				m_points;
+	BYTE *				m_linetypes;
+	int					m_rop[MAX_NUMBEROFLINES];
+	int					m_fadecount[MAX_NUMBEROFLINES];
+
+	POINT				m_lineStartPoint[MAX_NUMBEROFLINES];
+	POINT				m_lineEndPoint[MAX_NUMBEROFLINES];
+	LineTypes			m_lineType[MAX_NUMBEROFLINES];
+
+	int					m_penwidth[MAX_NUMBEROFLINES];
+	int					m_linecolorindex[MAX_NUMBEROFLINES];
+	int					m_colorindex;
+	int					m_currentpenwidth;
+	int					m_currentrop;
+	int					m_fadeseconds;
+
+	POINT				m_lineStartShiftPoint;
+
+	COLORREF			m_colors[10];
+
+	HCURSOR				m_hCursor;
+	HCURSOR				m_hPreviousCursor;
+
+	bool				m_bMarker;
+	int					m_oldpenwidth;
+	int					m_oldcolorindex;
+	int					m_oldrop;
+
+	bool				m_bInlineZoom;
+	POINT				m_ptInlineZoomStartPoint;
+	POINT				m_ptInlineZoomEndPoint;
+
+	static CHyperLink	m_link;
+};
---
+++ tags/Version-1.2/src/OptionsDlg.cpp
@@ -0,0 +1,99 @@
+#include "stdafx.h"
+#include "DemoHelper.h"
+#include "MainWindow.h"
+#include "registry.h"
+
+CHyperLink	CMainWindow::m_link;
+
+BOOL CALLBACK CMainWindow::OptionsDlgProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM /*lParam*/)
+{
+	switch (message) 
+	{
+	case WM_INITDIALOG:
+		{
+			CRegStdWORD regZoom(_T("Software\\DemoHelper\\zoomhotkey"), 0x331);
+			CRegStdWORD regDraw(_T("Software\\DemoHelper\\drawhotkey"), 0x332);
+			CRegStdWORD regCursor(_T("Software\\DemoHelper\\capturecursor"), TRUE);
+			CRegStdWORD regFadeSeconds(_T("Software\\DemoHelper\\fadeseconds"), 0);
+			SendMessage(GetDlgItem(hwndDlg, IDC_HOTKEY_ZOOMMODE), HKM_SETHOTKEY, (WPARAM)(DWORD)regZoom, 0);
+			SendMessage(GetDlgItem(hwndDlg, IDC_HOTKEY_DRAWMODE), HKM_SETHOTKEY, (WPARAM)(DWORD)regDraw, 0);
+			SendMessage(GetDlgItem(hwndDlg, IDC_CURSORCHECK), BM_SETCHECK, DWORD(regCursor) ? BST_CHECKED : BST_UNCHECKED, 0);
+
+			TCHAR buffer[128] = {0};
+			LoadString(hInst, IDS_WEBLINK, buffer, 128);
+			m_link.ConvertStaticToHyperlink(hwndDlg, IDC_WEBLINK, buffer);
+			_stprintf_s(buffer, 128, _T("%ld"), (DWORD)regFadeSeconds);
+			SetWindowText(GetDlgItem(hwndDlg, IDC_FADESECONDS), buffer);
+
+			// position the dialog box on the screen
+			HWND hwndOwner; 
+			RECT rc, rcDlg, rcOwner; 
+
+			hwndOwner = GetDesktopWindow(); 
+
+			GetWindowRect(hwndOwner, &rcOwner); 
+			GetWindowRect(hwndDlg, &rcDlg); 
+			CopyRect(&rc, &rcOwner); 
+
+			OffsetRect(&rcDlg, -rcDlg.left, -rcDlg.top); 
+			OffsetRect(&rc, -rc.left, -rc.top); 
+			OffsetRect(&rc, -rcDlg.right, -rcDlg.bottom); 
+
+			SetWindowPos(hwndDlg, HWND_TOP, rcOwner.left + (rc.right / 2), rcOwner.top + (rc.bottom / 2), 0, 0,	SWP_NOSIZE); 
+		}
+		break;
+	case WM_COMMAND: 
+		switch (LOWORD(wParam)) 
+		{
+		case IDOK: 
+			{
+				CRegStdWORD regZoom(_T("Software\\DemoHelper\\zoomhotkey"), 0x331);
+				CRegStdWORD regDraw(_T("Software\\DemoHelper\\drawhotkey"), 0x332);
+				CRegStdWORD regCursor(_T("Software\\DemoHelper\\capturecursor"), TRUE);
+				CRegStdWORD regFadeSeconds(_T("Software\\DemoHelper\\fadeseconds"), 0);
+				LRESULT res = SendMessage(GetDlgItem(hwndDlg, IDC_HOTKEY_DRAWMODE), HKM_GETHOTKEY, 0, 0);
+				regDraw = res;
+				res = SendMessage(GetDlgItem(hwndDlg, IDC_HOTKEY_ZOOMMODE), HKM_GETHOTKEY, 0, 0);
+				regZoom = res;
+				res = SendMessage(GetDlgItem(hwndDlg, IDC_CURSORCHECK), BM_GETCHECK, 0, 0);
+				regCursor = (res == BST_CHECKED ? TRUE : FALSE);
+				TCHAR buffer[128];
+				GetWindowText(GetDlgItem(hwndDlg, IDC_FADESECONDS), buffer, 128);
+				regFadeSeconds = _ttol(buffer);
+			}
+			// Fall through. 
+		case IDCANCEL: 
+			EndDialog(hwndDlg, wParam); 
+			return TRUE; 
+		} 
+	} 
+	return FALSE; 
+}
+
+WORD CMainWindow::HotKeyControl2HotKey(WORD hk)
+{
+	UINT flags = 0;
+	if (HIBYTE(hk) & HOTKEYF_ALT)
+		flags |= MOD_ALT;
+	if (HIBYTE(hk) & HOTKEYF_SHIFT)
+		flags |= MOD_SHIFT;
+	if (HIBYTE(hk) & HOTKEYF_EXT)
+		flags |= MOD_WIN;
+	if (HIBYTE(hk) & HOTKEYF_CONTROL)
+		flags |= MOD_CONTROL;
+	return MAKEWORD(LOBYTE(hk), flags);
+}
+
+WORD CMainWindow::HotKey2HotKeyControl(WORD hk)
+{
+	UINT flags = 0;
+	if (HIBYTE(hk) & MOD_ALT)
+		flags |= HOTKEYF_ALT;
+	if (HIBYTE(hk) & MOD_SHIFT)
+		flags |= HOTKEYF_SHIFT;
+	if (HIBYTE(hk) & MOD_WIN)
+		flags |= HOTKEYF_EXT;
+	if (HIBYTE(hk) & MOD_CONTROL)
+		flags |= HOTKEYF_CONTROL;
+	return MAKEWORD(LOBYTE(hk), flags);
+}
