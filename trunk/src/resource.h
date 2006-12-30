162e34f0761135a8394e23e11d47441e315821dc
tortoisesvn <tortoisesvn@7d1c1021-785a-45e7-86c9-21184ceb001a>
2006-12-30 14:27:03 +0000

    * Make the lines/shapes/arrows 'fade out' after a configured amount of seconds * update the version to 1.2

--- trunk/src/resource.h
+++ trunk/src/resource.h
@@ -20,6 +20,8 @@
 #define IDC_HOTKEY_DRAWMODE             1004
 #define IDC_CHECK1                      1005
 #define IDC_CURSORCHECK                 1005
+#define IDC_EDIT1                       1006
+#define IDC_FADESECONDS                 1006
 #define ID_TRAYCONTEXT_OPTIONS          32771
 #define ID_TRAYCONTEXT_ABOUT            32772
 #define ID_TRAYCONTEXT_ZOOM             32773
@@ -56,7 +58,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32804
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
