f7894631ad5765da31a5369a2ca96c7a827c4dae
XhmikosR <xhmikosr@gmail.com>
2013-04-28 16:29:03 +0000

    cosmetics: use the right case for the includes.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -19,7 +19,7 @@
 
 #include "stdafx.h"
 #include "MainWindow.h"
-#include "registry.h"
+#include "Registry.h"
 
 #define GET_X_LPARAM(lp) ((int)(short)LOWORD(lp))
 #define GET_Y_LPARAM(lp) ((int)(short)HIWORD(lp))
