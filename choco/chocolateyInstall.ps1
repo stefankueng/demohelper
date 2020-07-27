9b58382b6b9a0afaf6833c9679abb617f70187d6
Stefan Küng <stefankueng@gmail.com>
2020-07-27 19:29:41 +0200

    use ErrorActionPreference instead of try-catch

--- choco/chocolateyInstall.ps1
+++ choco/chocolateyInstall.ps1
@@ -1,14 +1,12 @@
+$ErrorActionPreference = 'Stop';
 $packageName = 'demohelper'
 $url64 = 'https://github.com/stefankueng/demohelper/releases/download/$MajorVersion$.$MinorVersion$.$MicroVersion$/DemoHelper-$MajorVersion$.$MinorVersion$.$MicroVersion$.zip'
 $checksum64 = '$checksum64$'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-try {
-  Install-ChocolateyZipPackage -PackageName '$packageName' `
-                               -UnzipLocation "$toolsDir" `
-                               -Url64bit "$url64" `
-                               -Checksum64 "$checksum64" `
-                               -ChecksumType64 "$checksumType64"
-} catch {
-  throw $_.Exception
-}
+
+Install-ChocolateyZipPackage -PackageName '$packageName' `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
